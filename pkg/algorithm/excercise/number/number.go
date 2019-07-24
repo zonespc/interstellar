7fde23f0b53ac697298d9da6d2eac70191d74727
“zhoujianwei” <zhoujianwei@zhihu.com>
2019-07-24 10:07:17 +0800

    add 正则表达式

--- pkg/algorithm/excercise/number/number.go
+++ pkg/algorithm/excercise/number/number.go
@@ -3,6 +3,7 @@ package number
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 //ReverseDigits 翻转32位整型数各位数字
@@ -110,3 +111,108 @@ func IsNumber(s string) bool {
 	return flag * res
 }
 */
+
+//IsPalindrome 判断一个整数是否是回文数
+func IsPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	a := []int{}
+	for x > 0 {
+		a = append(a, x%10)
+		x = x / 10
+	}
+	for i, j := 0, len(a)-1; i < j; {
+		if a[i] != a[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+//IsPalindromeByStr 判断一个整数是否是回文数
+func IsPalindromeByStr(x int) bool {
+	a := strconv.Itoa(x)
+	fmt.Println(a)
+	for i := 0; i < len(a)/2; i++ {
+		if a[i] != a[len(a)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+//IsMatchByRecall 回溯法判断是否匹配
+/*
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+说明:
+
+s 可能为空，且只包含从 a-z 的小写字母。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。\
+*/
+func IsMatchByRecall(s string, p string) bool {
+	if len(p) <= 0 {
+		return len(s) <= 0
+	}
+	firstMatch := (len(s) > 0) && (s[0] == p[0] || p[0] == '.')
+
+	if len(p) >= 2 && p[1] == '*' {
+		return IsMatchByRecall(s, p[2:]) || (firstMatch && IsMatchByRecall(s[1:], p))
+	}
+	return firstMatch && IsMatchByRecall(s[1:], p[1:])
+}
+
+//Map2DInt 二维整数集合
+type Map2DInt map[string]bool
+
+//Insert 二维整数集合插入操作
+func (m Map2DInt) Insert(a, b int, r bool) {
+	m[strconv.Itoa(a)+strconv.Itoa(b)] = r
+}
+
+//IsExist 判断是否存在
+func (m Map2DInt) IsExist(a, b int) bool {
+	_, ok := m[strconv.Itoa(a)+strconv.Itoa(b)]
+	return ok
+}
+
+//Get 返回元素
+func (m Map2DInt) Get(a, b int) (bool, bool) {
+	val, ok := m[strconv.Itoa(a)+strconv.Itoa(b)]
+	return val, ok
+}
+
+//IsMatchByDP 根据动态规划判断是否匹配
+func IsMatchByDP(s string, p string) bool {
+	m := Map2DInt{}
+	return dp(0, 0, s, p, m)
+}
+
+func dp(i, j int, s, p string, m Map2DInt) bool {
+	if !m.IsExist(i, j) {
+		ans := false
+		if j >= len(p) {
+			ans = i >= len(s)
+		} else {
+			firstMatch := i < len(s) && (s[i] == p[j] || p[j] == '.')
+			if j+1 < len(p) && p[j+1] == '*' {
+				ans = dp(i, j+2, s, p, m) || firstMatch && dp(i+1, j, s, p, m)
+			} else {
+				ans = firstMatch && dp(i+1, j+1, s, p, m)
+			}
+		}
+		m.Insert(i, j, ans)
+	}
+	val, ok := m.Get(i, j)
+	if ok {
+		return val
+	}
+	return false
+}
--- pkg/algorithm/excercise/number/number_test.go
+++ pkg/algorithm/excercise/number/number_test.go
@@ -13,3 +13,25 @@ func TestReverseDigits(t *testing.T) {
 func TestAtoi(t *testing.T) {
 	fmt.Println(Atoi("-91283472332"))
 }
+
+func TestIsPalindrome(t *testing.T) {
+	x := -122
+	fmt.Println(IsPalindrome(x))
+}
+
+func TestIsPalindromeByStr(t *testing.T) {
+	x := 121
+	fmt.Println(IsPalindromeByStr(x))
+}
+
+func TestIsMatchByRecall(t *testing.T) {
+	s := "ab"
+	p := ".*"
+	fmt.Println(IsMatchByRecall(s, p))
+}
+
+func TestIsMatchByDP(t *testing.T) {
+	s := "mississippi"
+	p := "mis*is*ip*."
+	fmt.Println(IsMatchByDP(s, p))
+}
